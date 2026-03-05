2640ac82a32b4dfaa37372671867b6e6be866619
Marshal38 <vladislav.zhukov52@gmail.com>
2026-03-05 23:09:28 +0500

    Make newtodo button open/close

--- src/TodoList/NewTodoButton.jsx
+++ src/TodoList/NewTodoButton.jsx
@@ -1,10 +1,35 @@
+import NewTodo from './NewTodo';
+import {useState} from 'react';
+
+
 function NewTodoButton() {
+
+  const [ open, setOpen] = useState(false)
+  
+  function handleNewTodo () {
+
+    setOpen((e) => !e)
+
+    
+    
+  }
+  
   return (
-    <button className="bg-violet-500 px-3 pb-1 rounded-full items-center text-violet-100 font-extralight text-3xl m-auto mr-9 mb-7
-    ">
+    <>
+    <button onClick={handleNewTodo} className="bg-violet-500 px-3 pb-1 rounded-full items-center text-violet-100 font-extralight text-3xl m-auto mr-9 mb-7
+    "> 
+
       +
     </button>
+    {open && <NewTodo/>}
+    </>
   )
 }
+
+
+
+
+
+
 
 export default NewTodoButton
